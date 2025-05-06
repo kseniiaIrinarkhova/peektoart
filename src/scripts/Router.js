aa97b219cf8054f84d179854033c2c1b51aa39ca
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2025-05-06 15:44:39 -0700

    added history state to nav method

--- src/scripts/Router.js
+++ src/scripts/Router.js
@@ -2,13 +2,38 @@
 const Router = {
     init: () =>{
         console.log("Running...");
+        //add event listener for all links in nav bar
         document.querySelectorAll('header a').forEach(link =>{
             link.addEventListener('click', e=>{
+                //prevent default action for link's clicking
                 e.preventDefault();
+                const url = e.target.getAttribute('href');
+                Router.nav(url);
             })
         })
+        //check if user click on back button on browser
+        window.addEventListener('popstate',(e)=>{
+            //call route
+            if(e.state === null){
+                //if state stack is empty go to the first page
+                Router.nav('/',true);
+            }else{
+                Router.nav(e.state.route,false);
+            }
+        });
     },
-    nav: ()=>{},
+    /*
+    function for navigating between routes
+    route - route name
+    addToHistory - parameter that helps user to go back 
+    */
+    nav: (route, addToHistory = true)=>{
+        //add information about routes to state
+        if(addToHistory){
+            history.pushState({route},null,route);
+        }
+
+    },
 }
 
 export default Router;
