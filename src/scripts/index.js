1705c0bebdac79c0c7c0f1975068766c40c30ada
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2025-05-06 14:46:19 -0700

    nav bar style

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,7 @@
-import * as bootstrap from 'bootstrap'
-console.log('testing')
+//add header style
+window.addEventListener("scroll", function(){
+    //get header element
+    let header = this.document.querySelector("header");
+    // add additional css class 'Sticky' while scrolling the page
+    header.classList.toggle("sticky", this.window.scrollY>0);
+});
